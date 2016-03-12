578200453b98abfc76dbd4504fa79fccd1399790
liqiang <785129039@qq.com>
2016-03-12 17:26:50 +0800

    jac init

---
+++ src/main/java/com/jonnyLee/jac/Jac.java
@@ -0,0 +1,16 @@
+package com.jonnyLee.jac;
+
+/**
+ * copyright    jonnyLee
+ * <pre>
+ *     author      jonnyLee
+ *     date        3/12/16
+ *     email       2355952129qq.com
+ *     desc
+ * </pre>
+ */
+public class Jac {
+
+
+
+}
--- src/test/java/com/jonnyLee/AppTest.java
+++ src/test/java/com/jonnyLee/AppTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2016 CTIM. All rights reserved.
+ */
+
 package com.jonnyLee;
 
 import junit.framework.Test;
